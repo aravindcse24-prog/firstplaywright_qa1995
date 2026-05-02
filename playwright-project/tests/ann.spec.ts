33f86450ca3ab424fcb624058506886439b5af54
Aravind <aravindcse24@gmail.com>
2026-05-02 18:44:03 +0530

    aravind first commit

---
+++ playwright-project/tests/ann.spec.ts
@@ -0,0 +1,14 @@
+import { test} from '@playwright/test';
+
+test('Login test-1', async ({ page }) => {
+console.log('test =1'); 
+});
+test('Login test-2', async ({ page }) => {
+console.log('test=2'); 
+});
+test('Login test-3', async ({ page }) => {
+console.log('test=3'); 
+});
+test('Login test-4', async ({ page }) => {
+console.log('test=5'); 
+});
